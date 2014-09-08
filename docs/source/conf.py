1284f7b7776f77f35d238bb81693854a12bc4d34
Koen Van Daele <koen.vandaele@rwo.vlaanderen.be>
2014-09-08 15:25:36 +0200

    Update doc build.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -36,8 +36,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.coverage',
     'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode',
-    'sphinxcontrib.httpdomain'
+    'sphinx.ext.viewcode'
 ]
 
 autoclass_content = 'both'
