60b43ba609461d23316c65eb710791276af02e23
Jonas Geuens <jonasgeuens@gmail.com>
2019-10-17 16:38:20 +0200

    Updated Python version
    Removed Python3.3 and Python3.4
    Added 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ requires = ['six']
 
 setup(
     name='language_tags',
-    version='0.4.6',
+    version='0.4.7',
     url='https://github.com/OnroerendErfgoed/language-tags',
     license='MIT',
     author='Flanders Heritage Agency',
@@ -31,9 +31,9 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License'
     ],
     platforms='any',
